e262468786c3378d86c3790ef83b5debddc26885
RAJESH NAIK <Rajesh.Naik@apexon.com>
2025-02-16 18:12:30 +0530

    added description

---
+++ src/components/company/companyDescription.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+export const CompanyDescription = ({description}) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleToggle = () => {
+      setIsExpanded(!isExpanded);
+    };
+  
+    return (
+      <div>
+        <p><strong>Description:</strong> 
+          {isExpanded ? (
+            description
+          ) : (
+            `${description.split(' ').slice(0, 10).join(' ')}...`
+          )}
+          <span onClick={handleToggle} style={{ color: 'blue', cursor: 'pointer' }}>
+            {isExpanded ? ' Read less' : ' Read more'}
+          </span>
+        </p>
+      </div>
+    );
+}
